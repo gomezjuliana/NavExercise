9310aa17edf040f32988b578dbed37d1b34a208a
Jules <jgmz.jg@gmail.com>
2017-06-14 11:57:16 -0500

    clean up menu function, add HTML and styles

--- public/js/nav.js
+++ public/js/nav.js
@@ -5,21 +5,25 @@ fetch('/api/nav.json')
 	console.log('oops!');
 });
 
-let list = document.querySelector('.labels'); // identifies the ul
-
 function createMenu(data){
-	data.items.forEach(element => printFirstLevel(element, list));
+	let list = document.querySelector('.labels'); // identifies the ul
+	data.items.forEach(element => printMenu(element, list, 'first-level'));
 }
 
-function printFirstLevel(element, container){
+function printMenu(element, container, newClass){
 	let li = document.createElement('li'); //creates a li
+	li.classList.add(newClass);
+	let a = document.createElement('a');
+	a.href = element.url;
+	li.appendChild(a);
 	let textLabel = document.createTextNode(element.label); // creates the text
-	li.appendChild(textLabel); // adds the text to the li
+	a.appendChild(textLabel); // adds the text to the li
 	container.appendChild(li); // adds the li to the ul
 
-	if (element.items.length && element.items.length > 0){
+	if (element.items && element.items.length > 0){
 		let ul = document.createElement('ul');
+		ul.classList.add('submenu')
 		li.appendChild(ul);
-		element.items.forEach(subElement => printFirstLevel(subElement, ul));
+		element.items.forEach(subElement => printMenu(subElement, ul, 'second-level'));
 	} 
 }
